5e786b88e980c5f49ab31333267f43fa74a4c87e
akira <99807453+akirauruma@users.noreply.github.com>
2024-01-06 22:59:35 +0300

    Add files via upload

---
+++ passwords/hashpass.py
@@ -0,0 +1,28 @@
+import hashlib
+import os
+
+password = "<PASSWORD>"
+salt = os.urandom(32)
+hashed_password = hashlib.pbkdf2_hmac(
+    'sha256',
+    password.encode('utf-8'),
+    salt,
+    100000,
+    dklen=128
+)
+
+try:
+    with open("hashed_password.txt", "r") as file:
+        lines = file.readlines()
+        if lines:
+            last_line = lines[-1]
+            user_id_counter = int(last_line.split('№:')[1].split('.\t')[0]) + 1
+        else:
+            user_id_counter = 1
+except FileNotFoundError:
+    user_id_counter = 1
+
+with open("hashed_password.txt", "a") as file:
+    file.write(f'User password: {hashed_password}')
+print(hashed_password)
+
